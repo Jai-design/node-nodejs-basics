a53772a3abbde3d0c7f117d74e062fea6e2ae985
Jamaica Harwell <jamaica.harwell91@outlook.com>
2024-10-01 18:22:49 -0500

    Update create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
 const create = async () => {
-    // Write your code here 
+    const fs = require('fs');
+    const path = require('path');
+function createFreshFile() {
+    const dir = path.join(__dirname, 'files');
+    const filePath = path.join(dir, 'fresh.txt');
+    const content = 'I am fresh and young';
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+    if (fs.existsSync(filePath)) {
+        throw new Error('FSoperation failed');
+    }
+    fs.writeFileSync(filePath, content, 'utf8');
+}
+    try {
+        createFreshFile();
+        console.log('File created successfully');
+    } catch (error) {
+        console.error(error.message);
+    }
 };
 
 await create();
